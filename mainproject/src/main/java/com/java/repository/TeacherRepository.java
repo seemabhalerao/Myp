88185ddbba2c4486ec65958041fd30df7896c048
seemabhalerao <122555072+seemabhalerao@users.noreply.github.com>
2023-01-17 18:06:07 +0530

    Delete mainproject directory

--- mainproject/src/main/java/com/java/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java.model.Teacher;
-
-public interface TeacherRepository extends JpaRepository<Teacher,Integer>{
-
-}
