2b465b937bc052e64b0c8b97f601c74a4ff9564e
seemabhalerao <122555072+seemabhalerao@users.noreply.github.com>
2023-01-17 18:16:44 +0530

    Delete mainproject directory

--- mainproject/src/main/java/com/java/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.java.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer>{
-
-}
